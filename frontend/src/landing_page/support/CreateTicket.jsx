65a5e5f7731febc661a1e2970d1032013384316c
Vivekchandra20 <3140vivekchandra@gmail.com>
2025-12-26 18:21:56 +0530

    Previous setup + working on Support page

---
+++ frontend/src/landing_page/support/CreateTicket.jsx
@@ -0,0 +1,20 @@
+function CreateTicket() {
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2 ">To create a ticket, select a relevant topic</h1>
+        <div className="col-4 p-5 mt-5 mb-5">
+          <h4 className="">
+            <i class="fa-solid fa-circle-plus"></i> Account opening
+          </h4>
+          Online Account Opening
+        </div>
+        <div className="col-1 p-5 mt-5 mb-5">
+          <h4 className="fs-2 text-centre"></h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateTicket;
---
+++ frontend/src/landing_page/support/SupportPage.jsx
@@ -0,0 +1,11 @@
+import CreateTicket from "./CreateTicket";
+
+function Support() {
+    return ( 
+        <>
+        <CreateTicket/>
+        </>
+     );
+}
+
+export default Support;
