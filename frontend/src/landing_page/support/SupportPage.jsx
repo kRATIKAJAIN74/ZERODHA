5988c1a49e29df79095ba54f9c48c368709ca844
Kratika Jain <jkratika8@gmail.com>
2025-12-26 20:34:01 +0530

    26/12/25_Kratika

--- frontend/src/landing_page/support/SupportPage.jsx
+++ frontend/src/landing_page/support/SupportPage.jsx
@@ -1,11 +1,18 @@
+import React from "react";
+
+import Hero from "./Hero";
 import CreateTicket from "./CreateTicket";
 
-function Support() {
-    return ( 
-        <>
-        <CreateTicket/>
-        </>
-     );
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <CreateTicket />
+    </>
+  );
 }
 
-export default Support;
+export default PricingPage;
